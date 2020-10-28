6d61019c673d89d5e70e767fb0ee687999b9ae28
Khalid <er.kmk123@gmail.com>
2020-10-28 11:39:37 +0530

    just change folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/pages/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
